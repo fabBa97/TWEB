4fa0ca51ab3c5fa65d2a2f7f740d813831cc4592
annafurciniti <63210203+annafurciniti@users.noreply.github.com>
2020-08-09 11:18:36 +0200

    maledetto commit2

--- Project/src/dao/Corso.java
+++ Project/src/dao/Corso.java
@@ -2,26 +2,26 @@ package dao;
 
 public class Corso {
     private String titolo;
-    private Integer id_docente;
+    private Integer id_corso;
 
 
-    public Corso(String titolo, Integer id_docente) {
+    public Corso(String titolo, Integer id_corso) {
         this.titolo = titolo;
-        this.id_docente = id_docente;
+        this.id_corso = id_corso;
     }
 
     public String getTitolo() { return titolo; }
 
-    public Integer getId_docente() { return id_docente; }
+    public Integer getId_corso() { return id_corso; }
 
 
     public void setTitolo(String titolo) { this.titolo = titolo; }
 
 
-    public void setId_docente(Integer id_docente) { this.id_docente = id_docente; }
+    public void setId_corso(Integer id_corso) { this.id_corso = id_corso; }
 
 
     @Override
-    public String toString() { return"corso: " + this.titolo;}
+    public String toString() { return titolo + " " + id_corso;}
 }
 
